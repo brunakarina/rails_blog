d463b252026c83bfa0fb35feb55e0973ac5840a7
Bruna Karina <brubskarina@gmail.com>
2024-05-19 20:42:07 -0300

    add tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+
+
+  ],
+  
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
 }
