c9dd6a360f3c7d6ea0ee847c2f43e6b74a481b0d
Bruna Karina <brubskarina@gmail.com>
2024-05-20 02:01:08 -0300

    add user to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_025843) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_044024) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -24,6 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_025843) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_025843) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
