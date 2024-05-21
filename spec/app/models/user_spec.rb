848953313f6057959c101fb39b0d5ab0dd538072
Bruna Karina <brubskarina@gmail.com>
2024-05-21 00:00:35 -0300

    add user spec

---
+++ spec/app/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe User, type: :model do
+    describe "creation" do
+        context "with valid attributes" do
+           user = User.new
+           user.email = "test.railsblog@email.com"
+           user.password = "test1234"
+           it "should save without errors" do
+               expect(user.save).to eq(true)
+           end
+        end
+
+        context "with invalid attributes" do
+            user = User.new
+            it "should not save if the email and password is not present" do
+                expect(user.save).to eq(false)
+            end
+
+            it "should not save if the email is not present" do
+                user.password = "test1234"
+                expect(user.save).to eq(false)
+            end
+
+            it "should not save if the password is not present" do
+                user.email = "test.railsblog@email.com"
+                user.password = nil
+                expect(user.save).to eq(false)
+            end
+
+            it "should not save if the email already exists" do
+                User.create(email: "test.railsblog@email.com", password: "test1234")
+                user.email = "test.railsblog@email.com"
+                user.password = "test1234"
+                expect(user.save).to eq(false)
+            end
+        end
+    end
+end
