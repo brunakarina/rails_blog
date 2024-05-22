9bf67724580432dfa5ec20fc9183cec9e7619a70
Bruna Karina <brubskarina@gmail.com>
2024-05-21 21:48:24 -0300

    add rich text

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates_presence_of :title, :content, :user_id
     has_many :comments, dependent: :destroy
     belongs_to :user
+    has_rich_text :content
 end
