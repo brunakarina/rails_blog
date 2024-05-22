601aa82a046377ec7624d7422a14b420fda8a702
Bruna Karina <brubskarina@gmail.com>
2024-05-21 21:07:04 -0300

    add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   broadcasts_to :post
+  validates_presence_of :content, :user_id
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    validates_presence_of :title, :content, :user_id
     has_many :comments, dependent: :destroy
     belongs_to :user
 end
